15bd2e488104a00e6973a096c372fff7e50913a2
Vladislav <V.Stoynov@campus.lmu.de>
2020-09-07 20:05:36 +0300

    data-preprocessing.py - Switched up the relabeling, use commentated one for no holes in any tool

--- tool_tracking/data-preprocessing.py
+++ tool_tracking/data-preprocessing.py
@@ -24,12 +24,6 @@ from datatools import ACC, GYR, MAG, MIC, POS, VEL
 from fhgutils import contextual_recarray_dtype
 
 
-
-relabel_actions_dict = {2:1,3:2,4:3,5:4,6:5,7:6,
-                        8:0,  # undefined AKA nothing is happening
-                        14:7,25:8,28:9,29:10,30:11,38:12}
-relabel_actions = np.vectorize( lambda x: relabel_actions_dict[x])
-
 def combine_sensors(reference_data, others, firstTimeStamp=np.Inf):
     """combine dataframes from different sensors.
     Resample all entries to the frequency of reference_data.
@@ -71,7 +65,7 @@ def extract_same_label(data, window_size):
     res = np.empty((0, window_size, data.shape[1]))
     i = 0
     while i + window_size < data.shape[0]:
-        candidate = data[i : i + window_size]
+        candidate = data[i: i + window_size]
         labels = candidate[:, -1]
         if len(np.unique(labels)) == 1:
             i += window_size
@@ -80,7 +74,7 @@ def extract_same_label(data, window_size):
                     res,
                     candidate.reshape(
                         (1, candidate.shape[0], candidate.shape[1])
-                    ),
+                    )
                 )
             )
         else:
@@ -88,7 +82,7 @@ def extract_same_label(data, window_size):
     return res
 
 
-def vote_label_time(window,majorityPortion = 0.5):
+def vote_label_time(window, majorityPortion=0.5):
     """
     Parameters
     ----------
@@ -99,14 +93,14 @@ def vote_label_time(window,majorityPortion = 0.5):
     -------
     the label that occured most in that window
     """
-    labels = np.unique(window[:,-1])
-    timePerLabel = np.empty((0,2))
-    diffs = np.vstack((np.diff(window[:,0]),window[:-1,-1])).T
+    labels = np.unique(window[:, -1])
+    timePerLabel = np.empty((0, 2))
+    diffs = np.vstack((np.diff(window[:, 0]), window[:-1, -1])).T
     for l in labels:
-        t = sum(diffs[diffs[:,-1] == l,0])
-        timePerLabel = np.vstack((timePerLabel,np.array([t,l]).reshape(1,-1)))
-    if np.max(timePerLabel[:,0]) > majorityPortion * sum(timePerLabel[:,0]):
-        return timePerLabel[timePerLabel[:,0] == np.max(timePerLabel[:,0]),-1].astype(int)[0]
+        t = sum(diffs[diffs[:, -1] == l, 0])
+        timePerLabel = np.vstack((timePerLabel, np.array([t, l]).reshape(1, -1)))
+    if np.max(timePerLabel[:, 0]) > majorityPortion * sum(timePerLabel[:, 0]):
+        return timePerLabel[timePerLabel[:, 0] == np.max(timePerLabel[:, 0]), -1].astype(int)[0]
     else:
         return -2  # inconsistent
 
@@ -127,19 +121,19 @@ def extract_by_time(data, length, overlap):
     """
     res = []
     res_labels = []
-    maxStamp = max(data[:,0])
-    currentStamp = min(data[:,0])
+    maxStamp = max(data[:, 0])
+    currentStamp = min(data[:, 0])
     # lastStamp = 0
     while currentStamp + length <= maxStamp:
-        candidate = data[np.logical_and((data[:,0] < currentStamp+length),(data[:,0] >= currentStamp))]
+        candidate = data[np.logical_and((data[:, 0] < currentStamp + length), (data[:, 0] >= currentStamp))]
         if candidate.size > 0:
             if vote_label_time(candidate) != -2:
-                res = res + [candidate[:,:-1]]
+                res = res + [candidate[:, :-1]]
                 res_labels = res_labels + [vote_label_time(candidate)]
-        currentStamp = data[data[:,0] >= currentStamp + (1-overlap)*length][0,0]
+        currentStamp = data[data[:, 0] >= currentStamp + (1 - overlap) * length][0, 0]
         # lastStamp = currentStamp + (1-overlap)*length
         # currentStamp = currentStamp + (1-overlap)*length
-    return res,res_labels
+    return res, res_labels
 
 
 mytool = "electric_screwdriver"
@@ -150,37 +144,47 @@ data_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", 
 mdr = MeasurementDataReader(source=data_filepath)
 data_dict = (mdr.query(query_type=Measurement).filter_by(Tool == mytool, DataTypes == [ACC, GYR, MIC, MAG]).get())
 
+relabel_dict = {  # Labels are shared across all tools, 'holes' in both pneumatics
+    2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6,
+    8: 0,  # undefined AKA nothing is happening
+    14: 7, 25: 8, 28: 9, 29: 10, 30: 11, 38: 12}
+
+# relabel_dict = {  # Each label has its own labels, no 'holes'
+#     8: 0, 7: 1,  # undefined and shaking exist for all tools
+#     2: 2,3: 3,4: 4,5: 5, #  shared across both screwdrivers
+#     25: 2, 28: 3, 29: 4, 30: 5  # rivet gun
+#     6: 6,  # manual_motor_rotation for electric screwdriver
+#     38: 6,  # impact exists for both pneumatics
+#     14: 7}  # tightening_double from both screwdrivers
+
+relabel_actions = np.vectorize(lambda x: relabel_dict[x])
+
 firstTimeStamp = np.Inf
-
-
 window_size = 20  # 60 is too big, @data_folder/info.md
 nr_features = 12  # hardcoded, idk. 2 + ACC:3, GYR:3, MIC:1, MAG:3
 
-data_windowed = np.empty((0,window_size,nr_features))
-data = np.empty((0,nr_features))
-for measurement_campaign in ["01","02","03","04"]:  # All recordings
-# for measurement_campaign in ["01"]:  # Only 1st recording
+data_windowed = np.empty((0, window_size, nr_features))
+data = np.empty((0, nr_features))
+for measurement_campaign in ["01", "02", "03", "04"]:  # All recordings
+    # for measurement_campaign in ["01"]:  # Only 1st recording
     acc = pd.DataFrame(data_dict.get(measurement_campaign).acc)
     gyr = pd.DataFrame(data_dict.get(measurement_campaign).gyr)
     mic = pd.DataFrame(data_dict.get(measurement_campaign).mic)
     mag = pd.DataFrame(data_dict.get(measurement_campaign).mag)
     # data_df, firstTimeStamp = combine_sensors(acc, [gyr], firstTimeStamp)  # Only ACC and GYR sensors
-    data_df, firstTimeStamp = combine_sensors(acc, [gyr,mic,mag], firstTimeStamp)  # All sensors, downsampled
+    data_df, firstTimeStamp = combine_sensors(acc, [gyr, mic, mag], firstTimeStamp)  # All sensors, downsampled
     # data_df, firstTimeStamp = combine_sensors(mic, [acc,gyr,mag], firstTimeStamp)  # All sensors, upsampled
     data_df['label'] = relabel_actions(data_df['label'])
-    data = np.vstack((data,data_df.values))
+    data = np.vstack((data, data_df.values))
     data_windowed = np.concatenate((data_windowed, extract_same_label(data_df.values, window_size)))
-
-
 
 X_windowed = data_windowed[:, :, :-1]
 y_windowed = data_windowed[:, 0, -1].astype(int)
-y_windowed_1hot = np.eye(max(y_windowed)+1)[y_windowed]
-
+y_windowed_1hot = np.eye(max(y_windowed) + 1)[y_windowed]
 
 # run for loop before that
 window_length = 20  # seconds
 overlap = 0.5  # portion
 # bottom are lists because windows have differing amount of stamps
-X_windowed_time, y_windowed_time = extract_by_time(data,window_length,overlap)
-y_windowed_time_1hot = np.eye(max(y_windowed_time)+1)[y_windowed_time]
+X_windowed_time, y_windowed_time = extract_by_time(data, window_length, overlap)
+y_windowed_time_1hot = np.eye(max(y_windowed_time) + 1)[y_windowed_time]
