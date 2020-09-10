5c511068f67a69343aa5f91575de3c22d45d4db0
Arber Qoku <arber.qoku@outlook.com>
2020-09-10 17:15:19 +0200

    Add TT implementation of LSTM cell Add corresponding test case

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorflow.keras import losses
 from tensorflow.keras import optimizers
 from tensorflow.keras.datasets import mnist
@@ -6,10 +7,13 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.models import Sequential
 
 from tool_tracking.tensor_train import TensorTrain
-from tool_tracking.layers import TTDense
+from tool_tracking.layers import TTDense, TTLSTMCell
 
 
-def test_mnist():
+tf.config.set_visible_devices([], "GPU")
+
+
+def test_mnist_dense():
     # load data
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -47,5 +51,50 @@ def test_mnist():
         validation_data=(x_test, y_test),
         verbose=1,
     )
+
+    assert history.history["val_accuracy"][-1] > 0.95
+
+
+def test_mnist_LSTM():
+    # load data
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    # standardize pixel values
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = Sequential()
+    # add tensor train dense layer
+    model.add(
+        tf.keras.layers.RNN(
+            TTLSTMCell(
+                tt=TensorTrain(
+                    input_shape=[2, 7, 2],
+                    output_shape=[4, 8, 4],
+                    ranks=[1, 4, 4, 1],
+                ),
+                activation="tanh",
+                kernel_initializer="glorot_normal",
+                bias_initializer=1e-3,
+            )
+        )
+    )
+
+    model.add(Dense(10, activation=None))
+
+    model.compile(
+        optimizer=optimizers.Adam(lr=1e-2),
+        loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    history = model.fit(
+        x_train,
+        y_train,
+        epochs=3,
+        batch_size=64,
+        validation_data=(x_test, y_test),
+        verbose=1,
+    )
+    model.summary()
 
     assert history.history["val_accuracy"][-1] > 0.95
--- tool_tracking/layers.py
+++ tool_tracking/layers.py
@@ -4,8 +4,11 @@ from typing import Dict
 from typing import List
 
 import tensorflow as tf
+from tensorflow.python.keras import backend as K
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import LSTMCell
+from tensorflow.python.ops import array_ops
 
 from tool_tracking.tensor_train import TensorTrain
 
@@ -102,3 +105,115 @@ class TTDense(Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape[0], self.output_dim
+
+
+class TTLSTMCell(LSTMCell):
+    """
+    A tensor train LSTM cell.
+    Adapted from https://github.com/tensorflow/tensorflow/blob/v2.3.0/tensorflow/python/keras/layers/recurrent.py
+    """
+
+    _counter = count(0)
+
+    def __init__(
+        self,
+        tt: TensorTrain,
+        activation: str = "tanh",
+        use_bias: bool = True,
+        kernel_initializer: str = "glorot_normal",
+        bias_initializer: float = 1e-2,
+        **kwargs: Dict
+    ):
+        """
+        Create a dense layer in tensor train format.
+
+        Parameters
+        ----------
+        tt : TensorTrain
+            A tensor train representation of the weight matrix.
+        activation : str
+            A standard activation function, e.g. 'relu'.
+        use_bias : bool
+            Whether to add a bias term to the computation.
+        kernel_initializer : str
+            A standard initializer for the weight matrix, e.g. 'glorot_normal'.
+        bias_initializer : float
+            TODO: A constant initializer for the bias.
+        kwargs : dict
+            Additional arguments accepted by tensorflow.keras.layers.Layer.
+        """
+        self.tt = tt
+        self.units = self.tt.matrix_shape[1] // 4
+        self.counter = next(self._counter)
+        self.activation = activation
+        self.use_bias = use_bias
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+
+        self._w = self.init_kernel()
+
+        self.compression_ratio = (
+            self.tt.matrix_shape[0]
+            * self.tt.matrix_shape[1]
+            / self.tt.num_params
+        )
+
+        super(TTLSTMCell, self).__init__(
+            self.units, name="tt_lstm_cell_{}".format(self.counter), **kwargs
+        )
+
+    def init_kernel(self) -> List[tf.Tensor]:
+        """
+        Initialize cores and return trainable tensors.
+
+        Returns
+        -------
+        list
+            A list of initialized cores
+        """
+        initializer = tf.keras.initializers.get(self.kernel_initializer)
+
+        def variable_initializer(shape):
+            return tf.Variable(
+                initializer(shape=shape), dtype="float32", trainable=True
+            )
+
+        return self.tt.init_cores(variable_initializer)
+
+    def build(self, input_shape):
+        if self.use_bias:
+            self.bias = self.add_weight(
+                shape=(self.units * 4,),
+                name="bias",
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint,
+            )
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 4),
+            name="recurrent_kernel",
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint,
+        )
+
+        self.built = True
+
+    def call(self, inputs, states, training=None):
+        """
+        Compute a forward pass of the given inputs.
+        """
+        h_tm1 = states[0]  # previous memory state
+        c_tm1 = states[1]  # previous carry state
+
+        z = self.tt.matmul(inputs)
+        z += K.dot(h_tm1, self.recurrent_kernel)
+        if self.use_bias:
+            z = K.bias_add(z, self.bias)
+
+        z = array_ops.split(z, num_or_size_splits=4, axis=1)
+        c, o = self._compute_carry_and_output_fused(z, c_tm1)
+        if self.activation is not None:
+            c = Activation(self.activation)(c)
+        h = o * c
+        return h, [h, c]
